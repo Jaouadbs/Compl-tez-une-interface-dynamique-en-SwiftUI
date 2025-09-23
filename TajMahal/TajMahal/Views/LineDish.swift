f1fcbd5996d305b5738cd081161a2492b5a6df44
Jaouadbs <wk.boussalah@gmail.com>
2025-09-23 20:24:25 +0200

    écran de démarrage et ligne de menu

---
+++ TajMahal/TajMahal/Views/LineDish.swift
@@ -0,0 +1,18 @@
+//
+//  LineDish.swift
+//  TajMahal
+//
+//  Created by Jaouad on 23/09/2025.
+//
+
+import SwiftUI
+
+struct LineDish: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LineDish()
+}
---
+++ TajMahal/TajMahal/Views/LineView.swift
@@ -0,0 +1,18 @@
+//
+//  LineView.swift
+//  TajMahal
+//
+//  Created by Jaouad on 23/09/2025.
+//
+
+import SwiftUI
+
+struct LineView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LineView()
+}
