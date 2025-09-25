ebaeeaeb3be4bc308cfa3fc3cab5db489d6914c6
Jaouadbs <wk.boussalah@gmail.com>
2025-09-25 15:44:03 +0200

    écran de démarrage et ligne de menu

---
+++ TajMahal/TajMahal/Views/DetailDish.swift
@@ -0,0 +1,18 @@
+//
+//  DetailDish.swift
+//  TajMahal
+//
+//  Created by Jaouad on 24/09/2025.
+//
+
+import SwiftUI
+
+struct DetailDish: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailDish()
+}
