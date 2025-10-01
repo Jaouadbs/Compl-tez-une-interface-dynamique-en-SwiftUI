7e81b4246641f4a274096a0c3f1d195cd72f760f
Jaouadbs <wk.boussalah@gmail.com>
2025-10-01 13:04:14 +0200

    ecran de detail de plat:  il manque le niveau d'epice Ecran de menu: il manque le niveau d'epice Ecran de menu a finaliser en custom

---
+++ TajMahal/TajMahal/ColorPalette.swift
@@ -0,0 +1,8 @@
+//
+//  ColorPalette.swift
+//  TajMahal
+//
+//  Created by Jaouad on 25/09/2025.
+//
+
+import Foundation
---
+++ TajMahal/TajMahal/UIColor+Extension.swift
@@ -0,0 +1,8 @@
+//
+//  UIColor+Extension.swift
+//  TajMahal
+//
+//  Created by Jaouad on 25/09/2025.
+//
+
+import Foundation
