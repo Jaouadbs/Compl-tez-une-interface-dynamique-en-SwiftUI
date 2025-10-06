7f87099e787decff0b2794cb85ee912192f72abd
Jaouadbs <wk.boussalah@gmail.com>
2025-10-06 16:25:18 +0200

    Ecran Menu: manque le CamelCase des titres des sections Entrées/ PlatsPrincipaux

---
+++ TajMahal/TajMahal/Views/SpiceIndicator.swift
@@ -0,0 +1,18 @@
+//
+//  SpiceIndicator.swift
+//  TajMahal
+//
+//  Created by Jaouad on 02/10/2025.
+//
+
+import SwiftUI
+
+struct SpiceIndicator: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SpiceIndicator()
+}
